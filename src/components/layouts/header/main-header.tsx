ec384658cf00984a735e0f964073eb8723c3cb4d
TM-ThaiTo <trinhto061003@gmail.com>
2025-05-06 08:26:31 +0700

    add button search

--- src/components/layouts/header/main-header.tsx
+++ src/components/layouts/header/main-header.tsx
@@ -4,24 +4,24 @@ import MenuBar from './menu-bar';
 import IconAction from './icon-action';
 
 export default function MainHeader() {
-    return (
-        <header>
-            <div className="flex h-[35px] items-center justify-between bg-[#0a58ca] px-4" />
-            <div className="flex h-[81px] items-center justify-between bg-white px-4 shadow-sm">
-                <div className="container flex items-center justify-between">
-                    <Link href="/" className="flex items-center">
-                        <Image
-                            src={'/logo-lib-hitu.png'}
-                            alt="HITU LIB"
-                            width={341}
-                            height={70}
-                            className="h-[65px] w-auto object-contain"
-                        />
-                    </Link>
-                    <MenuBar />
-                    <IconAction />
-                </div>
-            </div>
-        </header>
-    );
+  return (
+    <header>
+      <div className="flex h-[35px] items-center justify-between bg-[#0a58ca] px-4" />
+      <div className="flex h-[81px] items-center justify-between bg-white px-4 shadow-sm">
+        <div className="container flex items-center justify-between">
+          <Link href="/" className="flex items-center">
+            <Image
+              src={'/logo-lib-hitu.png'}
+              alt="HITU LIB"
+              width={341}
+              height={70}
+              className="h-[65px] w-auto object-contain"
+            />
+          </Link>
+          <MenuBar />
+          <IconAction />
+        </div>
+      </div>
+    </header>
+  );
 }
--- src/components/shares/icons.tsx
+++ src/components/shares/icons.tsx
@@ -1,14 +1,17 @@
-import { Search } from 'lucide-react';
+import { Search, X, ChevronRight, ArrowRight } from 'lucide-react';
 
 import { FaFacebookF } from 'react-icons/fa';
 import { AiOutlineX } from 'react-icons/ai';
 import { FaYoutube } from 'react-icons/fa6';
 
 const Icons = {
-    search: Search,
-    facebook: FaFacebookF,
-    spaceX: AiOutlineX,
-    youtube: FaYoutube,
+  search: Search,
+  close: X,
+  chevronRight: ChevronRight,
+  arrowRight: ArrowRight,
+  facebook: FaFacebookF,
+  spaceX: AiOutlineX,
+  youtube: FaYoutube,
 };
 
 export default Icons;
