ac5c6382d838745e687284e3476aa3c0eedb2535
TM-ThaiTo <trinhto061003@gmail.com>
2025-05-17 15:13:12 +0700

    fix slug

--- src/lib/slug.ts
+++ src/lib/slug.ts
@@ -2,11 +2,11 @@ import slugify from 'slugify';
 
 const mapSlug = (slug: string) =>
   slugify(slug, {
-    replacement: '-', // replace spaces with replacement character, defaults to `-`
-    remove: undefined, // remove characters that match regex, defaults to `undefined`
-    lower: true, // convert to lower case, defaults to `false`
-    strict: false, // strip special characters except replacement, defaults to `false`
-    locale: 'vi', // language code of the locale to use
+    replacement: '-',
+    remove: /[!@#$%^&*(),.?":{}|<>]/g,
+    lower: true,
+    strict: false,
+    locale: 'vi',
     trim: true,
   });
 
