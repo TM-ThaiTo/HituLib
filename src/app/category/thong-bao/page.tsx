83a79936a46c544bd6cf9e94c56af24c3d21cb7e
TM-ThaiTo <trinhto061003@gmail.com>
2025-05-22 07:06:48 +0700

    add item-category and fix all route category

--- src/app/category/thong-bao/page.tsx
+++ src/app/category/thong-bao/page.tsx
@@ -1,11 +1,9 @@
-import CustomLink from '@/hooks/next-link';
-import Image from 'next/image';
 import { getDataNews } from '@/constants/news';
 import { getFirstParamInt } from '@/lib/get-param';
-import { mapSlugWithId } from '@/lib/slug';
 import { SearchParams } from '@/types/search-params';
 import PaginationComponent from '@/components/shares/pagination';
 import { BreadcrumbWithCustomSeparator } from '@/components/shares/bread-crumb';
+import ItemCategory from '@/components/shares/item-category';
 
 const duongDan = '/category/thong-bao';
 
@@ -23,31 +21,13 @@ export default async function ThongBaoPage({ searchParams }: SearchParams) {
 
       <div className="grid grid-cols-1 gap-4">
         {data.items.map((item, index) => (
-          <div
-            key={index}
-            className="group flex gap-4 rounded-lg border border-gray-100 bg-white p-3 transition-all duration-300"
-          >
-            <div className="flex-shrink-0">
-              <CustomLink href={mapSlugWithId(item.tieuDe, item.id, 'category')}>
-                <Image
-                  width={200}
-                  height={120}
-                  src={item.anhDaiDien}
-                  alt={item.tieuDe}
-                  className="h-[120px] w-[200px] transform rounded-md transition-transform duration-300 group-hover:scale-105"
-                />
-              </CustomLink>
-            </div>
-            <div className="p-2">
-              <CustomLink href={`${mapSlugWithId(item.tieuDe, item.id)}`}>
-                <div className="cursor-pointer text-base font-bold transition-colors duration-300 group-hover:text-blue-600">
-                  {item.tieuDe}
-                </div>
-              </CustomLink>
-              <div className="mt-2 line-clamp-3 text-base font-light transition-colors duration-300">
-                {item.mota}
-              </div>
-            </div>
+          <div key={index}>
+            <ItemCategory
+              tieuDe={item.tieuDe}
+              id={item.id}
+              anhDaiDien={item.anhDaiDien}
+              moTa={item.mota}
+            />
           </div>
         ))}
       </div>
---
+++ src/app/category/thu-cam-on/page.tsx
@@ -0,0 +1,35 @@
+import ImagePublic from '@/config/image';
+import { BreadcrumbWithCustomSeparator } from '@/components/shares/bread-crumb';
+import ItemCategory from '@/components/shares/item-category';
+
+const data = [
+  {
+    id: 119,
+    tieuDe: 'Thư cảm ơn Thầy Vũ Nhật Tân đã tặng tài liệu cho thư viện',
+    mota: 'Thư cảm ơn Thầy Vũ Nhật Tân đã tặng tài liệu cho thư viện',
+    anhDaiDien: ImagePublic.thuCamOn,
+    ngayTao: '2025-03-01T00:00:00',
+    loaiId: 5,
+  },
+];
+
+export default function ThuCamOnPage() {
+  return (
+    <>
+      <BreadcrumbWithCustomSeparator />
+
+      <div className="grid grid-cols-1 gap-4">
+        {data.map((item, index) => (
+          <div key={index}>
+            <ItemCategory
+              tieuDe={item.tieuDe}
+              id={item.id}
+              anhDaiDien={item.anhDaiDien}
+              moTa={item.mota}
+            />
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
