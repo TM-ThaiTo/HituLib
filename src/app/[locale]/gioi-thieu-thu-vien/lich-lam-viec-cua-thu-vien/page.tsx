8772397b0b3bc64a8237cef1870c4308cb4f3997
TM-ThaiTo <trinhto061003@gmail.com>
2025-06-26 09:30:27 +0700

    fix logic code

--- src/app/[locale]/gioi-thieu-thu-vien/lich-lam-viec-cua-thu-vien/page.tsx
+++ src/app/[locale]/gioi-thieu-thu-vien/lich-lam-viec-cua-thu-vien/page.tsx
@@ -1,10 +1,16 @@
 import { LichLamViec } from '@/constants/scrape';
 import { WorkingHours } from '@/constants/scrape.en';
-import { getLocale } from 'next-intl/server';
+import { getLocale, getTranslations } from 'next-intl/server';
 import HtmlAvailable from '@/components/shares/html-available';
 
 export default async function LichLamViecPage() {
   const locale = await getLocale();
-  const content = locale === 'en' ? WorkingHours.contentHtml : LichLamViec.contentHtml;
-  return <HtmlAvailable content={content} />;
+  const t = await getTranslations('library_introduction');
+
+  const title = t('photos_of_activities');
+  const noContentMessage = t('no_content');
+  const content = locale === 'en' ? WorkingHours : LichLamViec;
+  return (
+    <HtmlAvailable title={title} content={content?.contentHtml} noContent={noContentMessage} />
+  );
 }
--- src/app/[locale]/gioi-thieu-thu-vien/noi-quy-thu-vien/page.tsx
+++ src/app/[locale]/gioi-thieu-thu-vien/noi-quy-thu-vien/page.tsx
@@ -1,10 +1,16 @@
 import { NoiQuyThuVien } from '@/constants/scrape';
 import { LibraryRegulations } from '@/constants/scrape.en';
-import { getLocale } from 'next-intl/server';
+import { getLocale, getTranslations } from 'next-intl/server';
 import HtmlAvailable from '@/components/shares/html-available';
 
 export default async function NoiQuyThuVienPage() {
   const locale = await getLocale();
-  const content = locale === 'en' ? LibraryRegulations.contentHtml : NoiQuyThuVien.contentHtml;
-  return <HtmlAvailable content={content} />;
+  const t = await getTranslations('library_introduction');
+
+  const title = t('photos_of_activities');
+  const noContentMessage = t('no_content');
+  const content = locale === 'en' ? LibraryRegulations : NoiQuyThuVien;
+  return (
+    <HtmlAvailable title={title} content={content?.contentHtml} noContent={noContentMessage} />
+  );
 }
